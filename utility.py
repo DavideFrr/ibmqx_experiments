b180d089562f851fa27cd5723abbd083354ae838
DavideFrr <davidefrr@gmail.com>
2018-01-08 11:05:31 +0100

    License

--- utility.py
+++ utility.py
@@ -1,9 +1,23 @@
-"""
+# Copyright 2017 Quantum Information Science, University of Parma, Italy. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# =============================================================================
 
-Author: Davide Ferrari
-August 2017
-
-"""
+__author__ = "Davide Ferrari"
+__copyright__ = "Copyright 2017, Quantum Information Science, University of Parma, Italy"
+__license__ = "Apache"
+__version__ = "2.0"
+__email__ = "davide.ferrari8@studenti.unipr.it"
 
 import logging
 import myLogger
